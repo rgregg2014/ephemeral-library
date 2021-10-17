6d06179e06ba3c48818978eca73a7aeea8f650cb
Rachel Gregg <rgregg2014@gmail.com>
2021-10-17 15:53:06 -0400

    added front-end components

--- client/src/components/BookForm/index.js
+++ client/src/components/BookForm/index.js
@@ -1,8 +1,19 @@
+import "./bookform.css";
+import { Button, Form } from "semantic-ui-react";
+
 const BookForm = () => {
   return (
-    <>
-      <h3>This is a form to add books.</h3>
-    </>
+    <Form>
+      <Form.Field>
+        <label>Title</label>
+        <input placeholder="Title" />
+      </Form.Field>
+      <Form.Field>
+        <label>Author</label>
+        <input placeholder="Author" />
+      </Form.Field>
+      <Button type="submit">Submit</Button>
+    </Form>
   );
 };
 
